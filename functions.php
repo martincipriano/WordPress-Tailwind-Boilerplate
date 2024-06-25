c975c6fd69e83c1d54116667d327bbcb3e495e3d
Jose Martin Cipriano <martin.c@scopicsoftware.com>
2024-06-25 21:53:29 +0800

    Add theme support for title tag and featured images

--- functions.php
+++ functions.php
@@ -6,6 +6,9 @@ add_action('wp_enqueue_scripts', function() {
 });
 
 add_action('after_setup_theme', function() {
+  add_theme_support('title-tag');
+  add_theme_support('post-thumbnails');
+
   register_nav_menu('header-navigation', 'Header Navigation');
   register_nav_menu('footer-navigation', 'Footer Navigation');
 });
